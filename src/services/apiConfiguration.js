17d5f69641feb38ebc28547684260e162fc6535e
Itay Neuberger <itayneu@gmail.com>
2021-12-28 10:00:09 +0200

    bug fix attempt

--- src/services/apiConfiguration.js
+++ src/services/apiConfiguration.js
@@ -6,7 +6,7 @@ export async function getLocationAutocomplete(query, apikey) {
   if (query && query.length > 0) {
     try {
       const response = await http.get(
-        `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?q=${query}&apikey=${apikey}`
+        `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?q=${query}&apikey=${apikey}`
       );
       return response.data;
     } catch (error) {
@@ -21,7 +21,7 @@ export async function getLocationAutocomplete(query, apikey) {
 export async function getCurrentWeather(locationKey, apikey) {
   try {
     const response = await http.get(
-      `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apikey}`
+      `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apikey}`
     );
     return response.data;
   } catch (error) {
@@ -32,7 +32,7 @@ export async function getCurrentWeather(locationKey, apikey) {
 export async function getFiveDayDailyForecast(locationKey, apikey) {
   try {
     const response = await http.get(
-      `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?metric=true&apikey=${apikey}`
+      `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?metric=true&apikey=${apikey}`
     );
     return response.data;
   } catch (error) {
